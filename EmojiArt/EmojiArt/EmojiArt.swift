edf9a25aa489ad3c1245899f2b7e17f283931229
bllhlskr <bllhlskr@gmail.com>
2021-05-04 01:06:51 +0300

    end of week 7

---
+++ EmojiArt/EmojiArt/EmojiArt.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by Halis  Kara on 3.05.2021.
+//
+
+import Foundation
+struct EmojiArt{
+    var backgroundURL : URL?
+    var emojis  = [Emoji]()
+    
+    
+    
+    
+    struct Emoji : Identifiable{
+        let text :String
+        var x :Int // offset from center
+        var y :Int// offset from center
+        var size :Int
+        let id : Int
+        
+        
+        fileprivate init(text:String,x:Int,y:Int,size:Int,id:Int){
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id 
+        }
+    }
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(_ text : String,x:Int,y:Int,size:Int){
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text:text,x:x,y:y,size:size,id:uniqueEmojiId))
+    }
+}
